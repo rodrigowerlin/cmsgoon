b4ebecb34ae4aa2f5666a7498d0bb3de5b0277bc
Rodrigo werlin <rodrigowerlin@gmail.com>
2017-06-26 16:25:54 -0300

    ajuste classe  util

---
+++ tests/index.php
@@ -0,0 +1,14 @@
+<?php
+
+include '../vendor/autoload.php';
+
+echo Cmsgoon\tools\Util::getMonthInWords(3);
+
+$arrParams = array();
+
+$arrParams['lg'] = 134;
+$arrParams['itens'][] = array("email" => "rodrigower@gmail.com", "codgrupo" => 1000);
+
+$arr = Cmsgoon\tools\Util::loadJson('http://localhost/ProjetosZend/sequenceweb/services/set-newsletters.php', $arrParams);
+
+print_r($arr);
